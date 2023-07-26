5de34edd074591ba226fffa90d5208cf6be1010a
Linas Norkus <linasnorkus92@gmail.com>
2023-07-26 21:55:55 +0300

    simple class library method "Greeting" to start off the skeleton code and continue running & creating simple tests

---
+++ DisplayMessages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibraryUnitTestDemoApp
+{
+    public class DisplayMessages
+    {
+        public string Greeting(string firstName, int hourOfTheDay)
+        {
+            string output = "";
+
+            if (hourOfTheDay < 12)
+            {
+                output = $"Good morning {firstName}";
+            }
+            else if (hourOfTheDay < 18)
+            {
+                output = $"Good afternoob {firstName}";
+            }
+            else 
+            {
+                output = $"Good afternoob {firstName}";
+            }
+            return output;
+        }
+    }
+}
+
