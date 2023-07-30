1279e031b0fc8b49568840ee889b72c2026bc408
Linas Norkus <linasnorkus92@gmail.com>
2023-07-30 21:04:45 +0300

    Wrote a few more tests just to feel more comfortable with them

---
+++ SimpleCalculatorApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibraryUnitTestDemoApp
+{
+    public class SimpleCalculatorApp
+    {
+        /*
+        public float UserInput(float input) 
+        {
+            bool isNumberValid;
+            float validNumber;
+
+            string stringInput = Console.ReadLine();
+
+            isNumberValid = float.TryParse(stringInput, out validNumber);
+
+            return validNumber;
+        }
+        */
+
+        public float Addition(float x, float y)
+        {
+            return x + y;
+        }
+
+        public float Subtraction(float x, float y)
+        {
+            return x - y;
+        }
+
+        public float Multiplication(float x, float y)
+        {
+            return x * y;
+        }
+
+        public float Division(float x, float y)
+        {
+            return x / y;
+        }
+
+    }
+}
+
